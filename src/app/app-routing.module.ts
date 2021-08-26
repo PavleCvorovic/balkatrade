b4178cd802e7a5dafdc2702756e3a36b61b82b38
trifunovicnikola <nikola.trifunovic@ict-investment.me>
2021-08-26 18:54:54 +0200

    dw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { HomepageComponent } from './homepage/homepage.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: 'homepage', component:HomepageComponent},
+  {path:'', redirectTo:'homepage', pathMatch:'full'},
   {path:'add', component:AddPostComponent}
 ];
 
